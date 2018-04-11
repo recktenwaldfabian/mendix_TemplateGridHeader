40a98daeb85ddffe9a41f8fc6f355968d936b230
recktenwaldf <recktenwaldf@mansystems.nl>
2018-04-11 11:14:34 +0200

    Make AppStore ready and add fix by Tobias Kuhn

--- src/TemplateGridHeader/widget/TemplateGridHeader.js
+++ src/TemplateGridHeader/widget/TemplateGridHeader.js
@@ -2,7 +2,6 @@
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
-
     "mxui/dom",
     "dojo/dom-style",
     "dojo/dom-attr",
@@ -20,32 +19,54 @@ define([
         refreshOnContextUpdate: false,
 
         // Internal
+        _observer: null,
+        _callback: null,
         contextObj: null,
+        gridObj: null,
+        headerObj: null,
+        
 
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
-
+            
+            // Setup callback function
+            this._callback = lang.hitch(this, function () 
+            {
+                this._onChange();
+            });
+            
             if (!this.refreshOnContextChange) {
                 this.executeCode();
             }
         },
 
+
         executeCode: function () {
             logger.debug(this.id + ".executeCode");
 
             mx.addOnLoad ( lang.hitch(this, function () {
-                setTimeout( lang.hitch(this, function () {
-                    this.moveHeader();
-                }),300);                    
+                //  Find objects
+                this.gridObj   = this.domNode.previousSibling;
+                this.headerObj = this.gridObj.previousSibling;
+                
+                // Hide header
+                domStyle.set(this.headerObj, "display", "none"); 
+                
+                // Look for dom changes in the grid object
+                this._observe(this.gridObj, this._callback);                 
             })); 
         },
 
+
         moveHeader: function() {
-            var grid = this.domNode.previousSibling;
-            var header = grid.previousSibling;
-            var target = grid.getElementsByClassName("mx-grid-toolbar")[0];
-            target.appendChild(header);
+            var target = this.gridObj.getElementsByClassName("mx-grid-toolbar")[0];
+            
+            target.appendChild(this.headerObj);
+            
+            // Show header
+            domStyle.set(this.headerObj, "display", "");
         },
+
 
         update: function (obj, callback) {
             logger.debug(this.id + ".update");
@@ -76,6 +97,64 @@ define([
             if (cb && typeof cb === "function") {
                 cb();
             }
+        },
+        
+        
+        /**
+         * Listens to DOM changes and fires the callback parameter each time
+         * 
+         * @param    The DOM node to listen to.
+         * @param    The function to call whenever the DOM changes.
+         */
+        _observe: function (obj, callback)
+        {
+            var MutationObserver       = (window.MutationObserver || window.WebKitMutationObserver);
+            var eventListenerSupported = window.addEventListener;
+            
+            if (MutationObserver) {
+                // Define a new observer
+                this._observer = new MutationObserver(function(mutations, observer) {
+                    if(mutations[0].addedNodes.length || mutations[0].removedNodes.length)
+                        callback();
+                });
+                // Have the observer observe foo for changes in children
+                this._observer.observe( obj, { childList:true, subtree:true });
+            }
+            else if(eventListenerSupported) {
+                obj.addEventListener('DOMNodeInserted', callback, false);
+                obj.addEventListener('DOMNodeRemoved', callback, false);
+                
+                this._observer = obj;
+            }
+        },
+        
+        
+        /**
+         * Triggered every time the observer triggers an event.
+         * 
+         * @param    The DOM node to listen to.
+         * @param    The function to call whenever the DOM changes.
+         */
+        _onChange: function (obj, callback)
+        {
+            if (this._observer == null) return;
+            
+            // Move the header
+            this.moveHeader();
+            
+            
+            // Stop observer
+            var MutationObserver = (window.MutationObserver || window.WebKitMutationObserver);
+            
+            if (this._observer instanceof MutationObserver) {
+                this._observer.disconnect();
+            }
+            else if (window.addEventListener) {
+                this._observer.removeEventListener('DOMNodeInserted', this._callback, false);
+                this._observer.removeEventListener('DOMNodeRemoved', this._callback, false);
+            }
+            
+            this._observer = null;
         }
     });
 });
